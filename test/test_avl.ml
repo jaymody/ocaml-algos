e491ff205c0c1cfb115381f3030f189c79c0b5e3
jaymody <jaykmody@gmail.com>
2023-08-02 20:49:52 -0700

    Add test for height.

--- test/test_avl.ml
+++ test/test_avl.ml
@@ -1,9 +1,16 @@
 open Algos
 module IntAvl = Avl.Make (Int)
 
+let rec calc_height = function
+  | IntAvl.Empty -> 0
+  | IntAvl.Node { l; r; _ } -> 1 + max (calc_height l) (calc_height r)
+;;
+
 let () =
   let open IntAvl in
   let list = List.init 100 (fun x -> x) in
   let tree = List.fold_left (fun tree num -> add num (-num) tree) empty list in
-  assert (to_list tree |> List.map fst = list)
+  assert (to_list tree |> List.map fst = list);
+  assert (calc_height tree < 10);
+  assert (calc_height tree = height tree)
 ;;
