2d831a6b1857d78b0e26bfbb6c30661c0fb10c9a
jaymody <jaykmody@gmail.com>
2023-08-04 14:57:07 -0700

    Add comments for each of the avl functions.

--- src/avl.ml
+++ src/avl.ml
@@ -1,6 +1,57 @@
 open Compare
 
-module Make (Key : Comparable) = struct
+(*
+   An AVL tree (self-balancing binary search tree).
+
+   Note, that there exists two functions to add key-value pairs into the tree,
+   insert and upsert. Insert allows adding duplicate entries, while upsert will
+   update the value of existing entries. You probably don't want to use these in
+   conjuction, as it is not guarenteed what key-value pair upsert will update.
+
+   The purpose of having both functions is so that we can reuse this same AVL
+   tree implementation for different data structures. For example, a map
+   (i.e. symbol table) can be implemented using only upsert. Likewise, a
+   priority queue requires only the use of insert, since we a pq must allow
+   duplicate entries.
+*)
+module Make (Key : Comparable) : sig
+  type key = Key.t
+
+  type 'a t =
+    | Empty
+    | Node of 'a t * key * 'a * 'a t * int
+
+  (* Returns an empty tree, which is used for initialization. *)
+  val empty : 'a t
+
+  (* Inserts the key-value pair into the tree. Duplicates keys are allowed, in
+     the event of two keys being equal, the new key-value pair will be added
+     to the left of the existing entry or entries. However, as things are added
+     and removed from the tree, it will rebalance so it is not guarenteed that
+     newer insertions will always be to the left of older ones. *)
+  val insert : key -> 'a -> 'a t -> 'a t
+
+  (* Insert or update the key-value pair into the tree. This differs from insert
+     because we do not allow duplicate keys, instead if the key already exists
+     in the tree, we simply update it's value. The functions insert and upsert
+     should not be used together, as there are no guarentees which key-value
+     pair will be updated in the event of duplicates. *)
+  val upsert : key -> 'a -> 'a t -> 'a t
+
+  (* Removes the key-value pair from the tree. If there is a matching key, we
+     return it's associated value inside a Some option (in addition to the new
+     tree with the key-value pair removed). Otherwise, we return None and the
+     unmodified tree. *)
+  val remove : key -> 'a t -> 'a option * 'a t
+
+  (* Finds the value associated with the key in the tree. If the value doesn't
+     exist, we return None, else we return a Some of the value. *)
+  val find : key -> 'a t -> 'a option
+
+  (* Returns the key-value pairs in the tree in a list sorted in
+     ascending order. *)
+  val to_list : 'a t -> (key * 'a) list
+end = struct
   type key = Key.t
 
   type 'a t =
@@ -9,12 +60,11 @@ module Make (Key : Comparable) = struct
 
   let empty = Empty
 
-  let height = function
-    | Empty -> 0
-    | Node (_, _, _, _, h) -> h
-  ;;
-
   let create l k v r =
+    let height = function
+      | Empty -> 0
+      | Node (_, _, _, _, h) -> h
+    in
     let make (l, k, v, r) = Node (l, k, v, r, 1 + max (height l) (height r)) in
     let rotate_left (k, v, l) (rl, rk, rv, rr) = make (l, k, v, rl), rk, rv, rr in
     let rotate_right (k, v, r) (ll, lk, lv, lr) = ll, lk, lv, make (lr, k, v, r) in
